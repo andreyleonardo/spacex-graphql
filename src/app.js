f8fdeb5c7cb76646dffb06e247cbd733f1f88d4a
Andrey Leonardo <andrey.leonardo@gmail.com>
2019-01-10 21:40:33 -0200

    Adds morgan for logging

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const graphqlRoute = require('./routes/graphql');
+
+const morganConfig = morgan(':method :url :status - :response-time ms');
 
 const app = express();
 
 app.use(
   bodyParser.json(),
   cors(),
+  morganConfig,
   graphqlRoute
 );
 
